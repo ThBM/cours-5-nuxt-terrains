385ed12ef416931ed0a5e56ed82b6797352a53ed
Thibault Barolat-Massole <thibault.barolat@outlook.fr>
2026-02-06 15:26:13 +0100

    Supabase

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,6 @@
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ["@nuxt/eslint", "@nuxt/ui"],
+  modules: ["@nuxt/eslint", "@nuxt/ui", "@nuxtjs/supabase"],
   css: ["~/assets/css/main.css"],
 });
